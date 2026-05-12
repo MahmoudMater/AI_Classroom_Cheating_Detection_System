bf347bd0c3a3ad86ce614300578e23521ab7f43f
mahmoudmatter12 <Mahmoud.Aziz-El-Din.3230080@CIT.hnu.edu.eg>
2026-05-12 16:56:54 +0300

    ai-service

--- ai-service/notebooks/README.md
+++
@@ -1,6 +0,0 @@
-# run:
-
-``` sh
-jupyter notebook notebooks/train_yolo_model1.ipynb
-```
-
--- scripte-to-upload.sh
+++
@@ -1,4 +0,0 @@
-while IFS=, read -r title body labels milestone
-do
- gh issue create --title "$title" --body "$body" --label "$labels"
-done < issues.csv
